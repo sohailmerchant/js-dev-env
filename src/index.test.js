4fbb70ffaaa4a62418b9e192fffd667302997e93
sohailmerchant <13290932+sohailmerchant@users.noreply.github.com>
2017-12-28 00:53:45 +0000

    fix text

--- src/index.test.js
+++ src/index.test.js
@@ -15,7 +15,7 @@ it('should say hello', (done)=>{
 const index = fs.readFileSync('./src/index.html', 'utf-8');
 jsdom.env(index, function (err, window){
 const h1 = window.document.getElementsByTagName('h1')[0];
-expect(h1.innerHTML).to.equal("Hello World 3");
+expect(h1.innerHTML).to.equal("Hello World!");
 done();
 window.close();
 });
