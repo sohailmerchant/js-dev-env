699d088347cac6bb393b3f7a1070f4d0891e4f93
sohailmerchant <13290932+sohailmerchant@users.noreply.github.com>
2017-12-28 00:35:38 +0000

    js-dev-env

---
+++ webpack.config.dev.js
@@ -0,0 +1,28 @@
+import path from 'path';
+
+export default {
+
+    debug: true,
+    devtool: 'inline-source-map',
+    noInfo: false,
+    entry: [
+        path.resolve(__dirname, 'src/index')
+
+    ],
+    target: 'web',
+    output:{
+
+        path: path.resolve(__dirname, 'src'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    plugin: [],
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_mdoules/, loaders: ['babel'] },
+            { test: /\.css$/, loaders: ['style', 'css'] }
+
+        ]
+
+    }
+}
